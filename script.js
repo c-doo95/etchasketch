479472a06bd444c6c6c41f76f9c78c4d909e8b11
c-doo95 <116585057+c-doo95@users.noreply.github.com>
2024-11-05 11:01:17 -0500

    create initial cell generation function

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+
+function createCell () {
+  const cell = document.createElement('div');
+  cell.id = 'cell';
+  container.appendChild(cell);
+};
