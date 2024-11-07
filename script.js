99ba347cdaf1f9dd9bf0a756ca921adc7ca2e39d
c-doo95 <116585057+c-doo95@users.noreply.github.com>
2024-11-07 11:19:55 -0500

    replace createCell func with rough createGrid func

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.querySelector('.container');
 
-function createCell () {
-  const cell = document.createElement('div');
-  cell.id = 'cell';
-  container.appendChild(cell);
+function createGrid(dimension) {
+  for (let i = 0; i < dimension; i++) {
+    const row = document.createElement('div');
+    row.id = 'row';
+    for (let i = 0; i < dimension; i++) {
+      const cell = document.createElement('div');
+      cell.id = 'cell';
+      row.appendChild(cell);
+    };
+    container.appendChild(row);
+  };
 };
+
+createGrid(5);
