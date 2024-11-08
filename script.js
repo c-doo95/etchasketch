c9508ecb01bf3447a202aeab51a70a834d5879b7
c-doo95 <116585057+c-doo95@users.noreply.github.com>
2024-11-08 12:47:52 -0500

    create colour change function and run on mouseover

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const container = document.querySelector('.container');
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function createGrid(dimension) {
   for (let i = 0; i < dimension; i++) {
     const row = document.createElement('div');
@@ -7,6 +16,9 @@ function createGrid(dimension) {
     for (let i = 0; i < dimension; i++) {
       const cell = document.createElement('div');
       cell.id = 'cell';
+      cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = getRandomColor();
+      });
       row.appendChild(cell);
     };
     container.appendChild(row);
